9b93014b9ea4dbbb7ff3a8f235b1b4bb363b4634
Theeraphat <theeraphat.tr@gmail.com>
2025-04-17 13:44:36 +0800

    Added fetchApplicationFromNRIC

--- control/HDBOfficerApplicationController.java
+++ control/HDBOfficerApplicationController.java
@@ -2,19 +2,42 @@ package control;
 
 import java.util.*;
 import entity.*;
+import repository.*;
 import utils.*;
 
 public class HDBOfficerApplicationController{
 
 	private static Scanner sc = new Scanner(System.in);
+
+	public static Application fetchApplicationFromNRIC(String NRIC){
+		List<User> users = UserService.getAllUsers();
+		User user = users.stream().filter(u -> u.getNric().equals(NRIC)).findFirst().orElse(null);
+		if(user != null && (user instanceof Applicant)) return ((Applicant) user).getApplication(); 
+		return null;
+	}
 	
-	public static void updateApplication()
+	public static void updateApplication(HDBOfficer officer)
 	{
-		System.out.print("Please enter applicant’s NRIC: ");
-        	String nric = sc.nextLine();
-        	// search for application by nric
-		application.getProject().addOccupiedFlat(application.getFlatType());
-		application.markBooked();
+		while(true){
+			System.out.print("Please enter applicant's NRIC: ");
+			String nric = sc.nextLine();
+			Application application = fetchApplicationFromNRIC(nric);
+			if(application == null){
+				System.out.print("Application was not found. Retry? (Y/N): ");
+                String retry = sc.nextLine();
+                if (retry.equalsIgnoreCase("N")) return;
+			}
+			else if(application.getProject() != officer.getAssignedProject()){
+				System.out.print("Officer is not responsible for the project. Retry? (Y/N): ");
+            	String retry = sc.nextLine();
+                if (retry.equalsIgnoreCase("N")) return;
+			}
+			else{
+				application.getProject().addOccupiedFlat(application.getFlatType());
+				application.markBooked();
+				break;
+			}
+		}
 		// generate receipt
 	}
 	
