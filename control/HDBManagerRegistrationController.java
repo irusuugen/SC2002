0cd279427bfdbf6b6b86b5279b5c1c6675879d98
pattanon-aem <pattanon.aem@gmail.com>
2025-04-18 11:33:47 +0800

    Update HDBManagerRegistrationController.java

--- control/HDBManagerRegistrationController.java
+++ control/HDBManagerRegistrationController.java
@@ -1,39 +1,47 @@
 package control;
 
 import entity.*;
+import java.util.*;
 
 public class HDBManagerRegistrationController {
-    public void approveOfficer(Project project, Registration registration) {
-        if (project.checkOfficerSlot()) {
-            project.addOfficer(registration.getRegisteredOfficer());
-            project.unregisterOfficer(registration);
-        } else {
-            System.out.println("No more officer slots available for this project.");
+    private Scanner sc = new Scanner(System.in);
+
+    public void handleRegistration(HDBManager manager, boolean approve) {
+        System.out.print("Enter project name: ");
+        String name = sc.nextLine();
+        for (Project project : manager.getCreatedProjects()) {
+            if (project.getProjectName().equalsIgnoreCase(name)) {
+                List<Registration> regs = project.getRegistrations();
+                for (int i = 0; i < regs.size(); i++) {
+                    System.out.printf("%d. %s\n", i + 1, regs.get(i).getRegisteredOfficer().getName());
+                }
+                int idx = IntGetter.readInt("Select officer: ") - 1;
+                Registration reg = regs.get(idx);
+                if (approve) {
+                    if (project.checkOfficerSlot()) {
+                        project.addOfficer(reg.getRegisteredOfficer());
+                        project.unregisterOfficer(reg);
+                        System.out.println("Officer approved.");
+                    } else {
+                        System.out.println("No slots available.");
+                    }
+                } else {
+                    project.unregisterOfficer(reg);
+                    System.out.println("Officer rejected.");
+                }
+            }
         }
     }
 
-    public void rejectOfficer(Project project, Registration registration) {
-        project.unregisterOfficer(registration);
-    }
-
-    public void approveApplication(Application application) {
-        Project project = application.getProject();
-        FlatType type = application.getFlatType();
-        if (project.getNumFlatAvailable(type) > 0) {
-            application.markSuccessful();
-            project.addOccupiedFlat(type);
-        } else {
-            application.markUnsuccessful();
+    public void approveWithdrawal(List<Application> allApplications) {
+        System.out.print("Enter applicant NRIC to approve withdrawal: ");
+        String nric = sc.nextLine();
+        for (Application app : allApplications) {
+            if (app.getApplicant().getNric().equalsIgnoreCase(nric)) {
+                app.withdraw();
+                app.getProject().removeOccupiedFlat(app.getFlatType());
+                System.out.println("Withdrawal approved.");
+            }
         }
-    }
-
-    public void rejectApplication(Application application) {
-        application.markUnsuccessful();
-    }
-
-    public void approveWithdrawal(Application application) {
-        application.withdraw();
-        FlatType type = application.getFlatType();
-        application.getProject().removeOccupiedFlat(type);
     }
 }
